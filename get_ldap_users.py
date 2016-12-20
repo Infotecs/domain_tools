c883dc96e450640520146dc21ad449a47455992e
Kirill V. Lyadvinsky <Kirill.Lyadvinsky@infotecs.ru>
2016-12-20 11:06:24 +0300

    pre test version

--- get_ldap_users.py
+++ get_ldap_users.py
@@ -1,39 +1,59 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+# Copyright (C) 2016 InfoTeCS JSC. All rights reserved.
+# Author: Kirill V. Lyadvinsky <Kirill.Lyadvinsky@infotecs.ru>
+#
+
 import sys
 import getpass
 from ldap3 import Server, Connection, Tls
 import ssl
+import csv
 
 
 def get_ldap_users():
     tls = Tls(validate=ssl.CERT_REQUIRED,
               version=ssl.PROTOCOL_TLSv1_2,
-              local_certificate_file='./infotecs_ldap.crt',
-              ca_certs_file='./infotecs_root_ca.pem')
-    s = Server('infotecs-nt', port=636, use_ssl=True, tls=tls)
+              local_certificate_file='./domain_ldap.crt',
+              ca_certs_file='./domain_root_ca.pem')
+    domain = input("Domain name [dc01.infotecs-nt]: ")
+    if domain == '':
+        domain = 'dc01.infotecs-nt'
+    s = Server(domain, port=636, use_ssl=True, tls=tls)
 
-    password = getpass.getpass("Please enter the ldap password: ")
-    c = Connection(s, user="infotecs-nt\\Lyadvinsky.Kir", password=password)
+    detected_username = getpass.getuser()
+    username = input("Domain user name [%s]: " % detected_username)
+
+    if username == '':
+        username = detected_username
+    password = getpass.getpass("Domain password: ")
+    c = Connection(s, user=username, password=password)
 
     if not c.bind():
         print('error in bind', c.result)
         return 0
 
+    # Map domain fields to the local structure
     fields = {'sAMAccountName': 'domain_name',
               'sn': 'surname',
               'givenName': 'name',
               'department': 'unit',
               'mail': 'email'}
     total_entries = 0
+    default_base = 'OU=Analytics,OU=TDC,OU=InfoTeCS,OU=Company,DC=infotecs-nt'
+    search_base = input("Enter search base [%s]: " % default_base)
+    if search_base == '':
+        search_base = default_base
     entry_generator = c.extend.standard.paged_search(
         # search_base is vary from
-        search_base='OU=Analytics,OU=TDC,OU=InfoTeCS,OU=Company,DC=infotecs-nt',
+        search_base=search_base,
         search_filter='(objectClass=person)',
         attributes=list(fields.keys()),
         paged_size=5,
         generator=True)
 
+    csv_file = open('./users.csv', 'w', newline='')
+    table = csv.writer(csv_file, delimiter=';')
     for entry in entry_generator:
         user = {}
         try:
@@ -41,11 +61,12 @@ def get_ldap_users():
                 user[fields[key]] = entry['attributes'][key][0]
         except KeyError:
             continue
-        print(user)
+        table.writerow(list(user.values()))
         total_entries += 1
-    print('Total entries retrieved:', total_entries)
+    csv_file.close()
+    print('Total entries retrieved: ', end='')
 
-    return 0
+    return total_entries
 
 
 def main():
